cec467c166789dab54e0c090a42f98759b0f9a59
Joseley Rebelo <joseley94@gmail.com>
2023-12-13 01:53:31 +0000

    Improved react test

--- tests/main.test.tsx
+++ tests/main.test.tsx
@@ -2,36 +2,40 @@ import "@testing-library/jest-dom";
 import { newSpot } from "../src/newSpot";
 import { render, screen } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
+import { useEffect } from "react";
 
 describe("newSpot", () => {
   const { ContextProvider: Spot, useContext: useSpot } = newSpot(
     { states: { exampleState: "initial" } },
     {
-      getExampleState: function (ctx) {
-        return ctx.states.exampleState;
+      getExampleState: function (context) {
+        return context.states.exampleState;
       },
     }
   );
 
-  const AutoUpdater = () => {
-    useSpot().setStates.setExampleState("overwrite");
+  const AutoUpdater = ({ value }: { value: string }) => {
+    const spot = useSpot();
+    // Updates state on component mount
+    useEffect(() => spot.setStates.setExampleState(value), [spot]);
     return <></>;
   };
 
   const Printer = () => {
-    return <>{useSpot().states.exampleState}</>;
+    // Prints the react state
+    return <>{useSpot().getExampleState()}</>;
   };
 
-  it("Updates states on sibling component.", async () => {
+  it("Updates states on sibling component (expects 'overwritten' instead of 'initial')", async () => {
     await act(async () => {
       render(
         <Spot>
           <Printer />
-          <AutoUpdater />
+          <AutoUpdater value="overwritten" />
         </Spot>
       );
     });
 
-    expect(screen.getByText("overwrite")).toBeInTheDocument();
+    expect(screen.getByText("overwritten")).toBeInTheDocument();
   });
 });
